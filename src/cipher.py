90092ce1f32aec1ad52d4807a5e69722c4fa6b2d
ADITYA KUMAR <adtkmr.contact@gmail.com>
2026-01-29 14:08:26 +0530

    Update cipher.py

--- src/cipher.py
+++ src/cipher.py
@@ -24,3 +24,18 @@ key = random_key()
 
 key['a'], key['z']
 
+
+
+
+
+
+
+def encrypt(plaintext,key):
+  return ''.join(key.get(c,c) for c in plaintext.lower())
+
+
+def decrypt(ciphertext,key):
+  inverse_key = { v: k for k, v in key.items()}
+
+  return ''.join(inverse_key.get(c, c) for c in ciphertext.lower())
+
